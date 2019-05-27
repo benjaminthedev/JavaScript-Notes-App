7cb6036590a8ca3cf057d92d4891fc8e3b1ff57a
Benjamin Dordoigne <benjamin.d@onthegosystems.com>
2019-05-27 06:44:57 +0100

    Updated Func

--- notes-functions.js
+++ notes-functions.js
@@ -1,14 +1,7 @@
 // Read existing notes from localStorage
 const getSavedNotes = () => {
     const notesJSON = localStorage.getItem('notes')
-
-    return notesJSON !== null ? JSON.parse(notesJSON) : []
-    
-    // if (notesJSON !== null) {
-    //     return JSON.parse(notesJSON)
-    // } else {
-    //     return []
-    // }
+    return notesJSON !== null ? JSON.parse(notesJSON) : []   
 }
 
 // Save the notes to localStorage
